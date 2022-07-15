e381e2746aa91bcca7c39dfe8520ef502ef77ae5
mercywanjugu <mercy.wanjugu@student.moringaschool.com>
2022-07-15 14:46:03 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,5 @@
+
+
 import React from "react";
 
 function NavBar() {
@@ -16,6 +18,14 @@ function Home() {
   );
 }
 
+function About() {
+  return (
+    <div id="about">
+      <h2>About.</h2>
+    </div>
+  )
+}
+
 {/* write an <About> component here */}
 
 
@@ -23,8 +33,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
